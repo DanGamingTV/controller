f12626291257482e3871eb9032d840929e71e7f0
DanGamingTV <36214413+DanGamingTV@users.noreply.github.com>
2020-06-01 22:25:12 +0000

    Update ev3.py

--- hardware/ev3.py
+++ hardware/ev3.py
@@ -10,7 +10,9 @@ from ev3dev2.button import Button
 from ev3dev2.sound import Sound
 from ev3dev2.power import PowerSupply
 from time import sleep
+import robot_util
 import threading
+import importlib
 tts_module = importlib.import_module('tts.ev3')
 
 STUD_MM = 8
@@ -31,6 +33,7 @@ supply = PowerSupply()
 mdiff.odometry_start()
 scriptname = "/hardware/ev3.py"
 debug = True
+stationary_mode = False
 
 movements = []
 # drive in a turn for 5 rotations of the outer motor
@@ -42,15 +45,17 @@ def debug_log(message):
         print(message)
 
 # drive in a different turn for 3 seconds
-def setup():
+def setup(robot_config):
     global speed
-    speed = 45
+    speed = 70
     global travel #mm, distance to travel forwards and backwards
     travel = 100
     global brake
     brake = True
     global turn_degrees
-    turn_degrees = 30
+    turn_degrees = 32.5
+    global stationary_mode
+    stationary_mode = False
 def forward():
     thing = 1
     #mdiff.on_for_distance(SpeedRPM(speed), travel)
@@ -108,6 +113,10 @@ def right():
     else:
         movements.append("r")
 
+def connection(source):
+    global scriptname
+    print("{}: connected to {}".format(scriptname, source))
+
 def go_home():
     reverse = movements[::-1]
     for i in range(len(reverse)):
@@ -149,36 +158,73 @@ def check_battery():
     threading.Timer(20, check_battery).start()
     if (supply.measured_volts < 6):
         if (supply.measured_volts > 5.5):
-            tts("Battery getting low, {} percent battery left.".format(volt_to_percent(supply.measured_volts)))
+            tts_module.say("Battery getting low, {} percent battery left.".format(volt_to_percent(supply.measured_volts)))
         else:
-            tts("Battery critically low, {} percent battery left.".format(volt_to_percent(supply.measured_volts)))
+            tts_module.say("Battery critically low, {} percent battery left.".format(volt_to_percent(supply.measured_volts)))
 
 def lights_on():
     lightswitch.on_to_position(30, -28)
 def lights_off():
     lightswitch.on_to_position(30, 0)
 
+def stationary_on():
+    global stationary_mode
+    stationary_mode = True
+    debug_log("ran stationary_on()")
 
+def stationary_off():
+    global stationary_mode
+    stationary_mode = False
+    debug_log("ran stationary_off()")
 
 def move(args):
     print(args)
-    if (args['button']['command'] == 'u'):
+    global stationary_mode
+    if (args['button']['command'] == 'u' and stationary_mode == False):
         forward()
-    elif (args['button']['command'] == 'd'):
+    elif (args['button']['command'] == 'd' and stationary_mode == False):
         backwards()
-    elif (args['button']['command'] == 'l'):
+    elif (args['button']['command'] == 'l' and stationary_mode == False):
         left()
-    elif (args['button']['command'] == 'r'):
+    elif (args['button']['command'] == 'r' and stationary_mode == False):
         right()
-    elif (args['button']['command'] == 'arm_up'):
+    elif (args['button']['command'] == 'arm_up' and stationary_mode == False):
         arm_up()
-    elif (args['button']['command'] == 'arm_down'):
+    elif (args['button']['command'] == 'arm_down' and stationary_mode == False):
         arm_down()
-    elif (args['button']['command'] == 'home'):
+    elif (args['button']['command'] == 'home' and stationary_mode == False):
         go_home()
     elif (args['button']['command'] == 'lights_on'):
         lights_on()
     elif (args['button']['command'] == 'lights_off'):
         lights_off()
+    elif (args['button']['command'] == 'clear_buffer'):
+        movements.clear()
+        tts_module.say("Movement buffer cleared.")
+        robot_util.sendChatMessage("Movement buffer cleared.")
+   #elif (args['button']['command'] == 'speed_up'):
+    #   speed_up()
+   #elif (args['button']['command'] == 'speed_down'):
+    #   speed_down()
+
+
 
 check_battery()
+
+
+#btn.on_up(forward)
+#btn.on_down(backwards)
+#while True:
+    #if (btn.check_buttons(['up'])):
+        #forward()
+    #elif (btn.check_buttons(['down'])):
+        #backwards()
+
+#sense.distance_centimeters_continuous()
+#while True:
+#    if us.distance_centimeters < 8: # to detect objects closer than 40cm
+#        # In the above line you can also use inches: us.distance_inches < 16
+#        backwards()
+
+#    sleep (0.5) # Give the CPU a rest
+#print(us.distance_centimeters)
